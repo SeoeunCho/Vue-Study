627183345718172fb088d0c048cfa52b9e226134
seoeuncho <westsilver24@naver.com>
2023-07-19 01:34:34 +0900

    Vue Blog 초기커밋 (+Bootstrap)

---
+++ vuelog/src/components/List.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mt-4">
+    <h5>{{ writeData.title }}</h5>
+    <p>{{ writeData.content }}</p>
+    <p>{{ writeData.data }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    writeData: Object,
+  },
+};
+</script>
+
+<style></style>
