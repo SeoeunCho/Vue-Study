59c95034046ac4fc070946ae1ed3ec2255a3abc7
seoeuncho <westsilver24@naver.com>
2023-07-20 20:27:50 +0900

    style 속성 데이터 바인딩 (CamelCase, 백틱)

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post v-for="(item, i) in postsData" :post="item" :key="i" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+  name: "Container",
+  props: {
+    postsData: Array,
+  },
+  components: {
+    Post,
+  },
+};
+</script>
+
+<style></style>
