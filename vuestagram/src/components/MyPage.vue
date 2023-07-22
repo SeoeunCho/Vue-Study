1661a93cc48d8607a6477938abf8baf1ac65bc0f
seoeuncho <westsilver24@naver.com>
2023-07-23 04:24:11 +0900

    Composition API 함수사용 - 검색기능 구현

--- vuestagram/src/components/MyPage.vue
+++ vuestagram/src/components/MyPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="padding: 10px">
     <h4>팔로워</h4>
-    <input placeholder="🔍" />
+    <input placeholder="🔍" @input="search($event.target.value)" />
     <div class="post-header" v-for="(item, i) in follower" :key="i">
       <div
         class="profile"
@@ -13,45 +13,43 @@
 </template>
 
 <script>
-import { onMounted, ref, reactive, toRefs, watch, computed } from "vue";
+import { onMounted, ref } from "vue";
 import axios from "axios";
-import { useStore } from "vuex";
+// import { useStore } from "vuex";
 
 export default {
   name: "MyPage",
   props: {
     one: Number,
   },
-  setup(props) {
-    // = created
+  setup() {
+    // setup() = created
     // ref() : primitive 자료형 (array, object 이외의 자료형 / 엄격하지않음 / 써도 됨)
-    let follower = ref([]);
     // reactive() : array, object 자료형
-    let test = reactive({ name: "Cho" });
-
-    let { one } = toRefs(props);
-    console.log(one.value);
-
-    watch(one, () => {});
-
-    let result = computed(() => {
-      // created전에 실행됨
-      return follower.value.length;
-    });
-    console.log(result.value);
-
-    let store = useStore();
-    console.log(store.state.name);
+    let follower = ref([]);
+    let followerOrg = ref([]);
 
     onMounted(() => {
       axios.get("/follower.json").then((res) => {
         follower.value = res.data;
+        followerOrg.value = [...res.data];
       });
     });
 
+    const search = (e) => {
+      if (e) {
+        follower.value = followerOrg.value.filter((el) => {
+          return el.name.indexOf(e) != -1;
+        });
+      } else {
+        follower.value = followerOrg.value;
+      }
+    };
+
     return {
       follower,
-      test,
+      followerOrg,
+      search,
     };
   },
   data() {
